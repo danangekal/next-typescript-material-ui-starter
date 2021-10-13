3aa02c38b4b9211ba75f5dfbf3beeeac2f7ec01f
Danang Eko Alfianto <danangekal@gmail.com>
2021-10-13 08:56:55 +0700

    perf: update some deps (next, next pwa, material-ui, eslint)

--- components/theme.ts
+++ components/theme.ts
@@ -1,7 +1,8 @@
-import { createMuiTheme, Theme } from '@material-ui/core';
+import { Theme } from '@material-ui/core';
 import { red } from '@material-ui/core/colors';
+import { createTheme } from '@material-ui/core/styles';
 
-const theme: Theme = createMuiTheme({
+const theme: Theme = createTheme({
   palette: {
     primary: {
       main: '#556cd6',
